fae2ea6b9ec89e9841c107fb8d2da73b1815c869
djeon <du0928@naver.com>
2021-06-15 19:15:05 +0900

    test

--- minishell/minishell.c
+++ minishell/minishell.c
@@ -6,26 +6,50 @@
 /*   By: djeon <djeon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/14 19:52:35 by djeon             #+#    #+#             */
-/*   Updated: 2021/06/14 20:08:30 by djeon            ###   ########.fr       */
+/*   Updated: 2021/06/15 16:37:08 by djeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "minishell.h"
-#include <readline/readline.h>
-#include <readline/history.h>
+#include "minishell.h"
 
-
-int				main(void)
+int				main(int argc, char *argv[], char *envp[])
 {
 	char		*line;
+	char		*path;
+	int			status;
+	pid_t		pid;
+	pid_t		wait_pid;
 
-	line = readline("minishell :");
-/*
-	ft_putstr_fd("minishell $ ", 1);
-	while (get_next_line(0, &line) > -1)
+//	argv[0] = "ls";
+//	argv[1] = NULL;
+	argc = 1;
+	while ((line = readline("minishell $ ")) != NULL)
 	{
-		ft_putstr_fd("minishell $ ", 1);
+		path = NULL;
+		pid = -1;
+		if (ft_strncmp("ls", line, 2) == 0)
+		{
+			pid = fork();
+			path = "/bin/ls";
+		}
+		else
+			ft_putstr_fd("command not found\n", 1);
+		if (path != NULL)
+		{
+			if (pid == 0)
+			{
+				execve(path, argv, envp);
+				path = NULL;
+				return (0);
+			}
+			else
+			{
+				while ((((wait_pid = wait(&status)) == -1) && errno == EINTR))
+				{
+				}
+			}
+		}
 	}
-*/
 	add_history(line);
+	return (0);
 }
--- minishell/minishell.h
+++ minishell/minishell.h
@@ -6,7 +6,7 @@
 /*   By: djeon <djeon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/14 19:54:23 by djeon             #+#    #+#             */
-/*   Updated: 2021/06/14 19:58:41 by djeon            ###   ########.fr       */
+/*   Updated: 2021/06/15 16:27:36 by djeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,10 @@
 
 # include "./get_next_line/get_next_line.h"
 # include "./libft/libft.h"
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <errno.h>
+
+# include <stdio.h>
 
 #endif
