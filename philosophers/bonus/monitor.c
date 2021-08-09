33c68c7f228faa7034ff1f7cbd114bc0bb67d304
Your Name <you@example.com>
2021-08-09 17:56:03 +0900

    norminette v3

--- philosophers/bonus/monitor.c
+++ philosophers/bonus/monitor.c
@@ -6,16 +6,16 @@
 /*   By: djeon <djeon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/08 17:34:47 by djeon             #+#    #+#             */
-/*   Updated: 2021/08/08 18:23:36 by djeon            ###   ########.fr       */
+/*   Updated: 2021/08/09 17:55:16 by djeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers_bonus.h"
 
-int process_state(t_carry *carrier, int *full)
+int	process_state(t_carry *carrier, int *full)
 {
-	int i;
-	int status;
+	int	i;
+	int	status;
 
 	i = -1;
 	while (++i < carrier[0].con.num_of_philo)
@@ -55,15 +55,15 @@ void monitor_process(t_carry *carrier)
 
 void *monitor_thread(void *data)
 {
-	struct timeval cur;
-	t_carry *carrier;
-	long time;
+	struct timeval	cur;
+	t_carry			*carrier;
+	long			time;
 
 	carrier = (t_carry *)data;
 	while (1)
 	{
-		if (carrier->con.num_of_eat != -1 && carrier->cur_num_eating >=
-				carrier->con.num_of_eat)
+		if (carrier->con.num_of_eat != -1 && carrier->cur_num_eating >= \
+			carrier->con.num_of_eat)
 		{
 			sem_post(carrier->sem);
 			exit(0);
