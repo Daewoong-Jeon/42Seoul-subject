221c0a5c68c62e16730cc0b63bde168a2e8ca559
djeon <du0928@naver.com>
2021-08-12 10:57:55 +0900

    fix error

--- philosophers/philo/main.c
+++ philosophers/philo/main.c
@@ -6,33 +6,45 @@
 /*   By: djeon <djeon@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/19 17:27:21 by djeon             #+#    #+#             */
-/*   Updated: 2021/08/11 23:36:38 by djeon            ###   ########.fr       */
+/*   Updated: 2021/08/12 10:55:02 by djeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
+int		routine(t_carry *carrier)
+{
+	int	num_of_philo;
+
+	num_of_philo = carrier->con.num_of_philo;
+	if (pick_up(carrier, &carrier->fork[carrier->philo],
+			&carrier->fork[(carrier->philo + 1) % num_of_philo]) == -1)
+		return (-1);
+	if (eating(carrier) == -1)
+	{
+		put_down(&carrier->fork[carrier->philo],
+			&carrier->fork[(carrier->philo + 1) % num_of_philo]);
+		return (-1);
+	}
+	put_down(&carrier->fork[carrier->philo],
+		&carrier->fork[(carrier->philo + 1) % num_of_philo]);
+	if (sleeping_and_thinking(carrier) == -1)
+		return (-1);
+	return (0);
+}
+
 void	*exec(void *data)
 {
 	t_carry		*carrier;
 	pthread_t	monitor_id;
-	int			num_of_philo;
 
 	carrier = (t_carry *)data;
-	num_of_philo = carrier->con.num_of_philo;
 	pthread_create(&monitor_id, NULL, monitor, carrier);
 	if (carrier->philo % 2 == 0)
 		waiting(carrier, carrier->con.start, carrier->con.time_to_eat / 2);
 	while (1)
 	{
-		if (pick_up(carrier, &carrier->fork[carrier->philo],
-				&carrier->fork[(carrier->philo + 1) % num_of_philo]) == -1)
-			return (NULL);
-		if (eating(carrier) == -1)
-			return (NULL);
-		put_down(&carrier->fork[carrier->philo],
-			&carrier->fork[(carrier->philo + 1) % num_of_philo]);
-		if (sleeping_and_thinking(carrier) == -1)
+		if (routine(carrier) == -1)
 			return (NULL);
 		usleep(500);
 	}
